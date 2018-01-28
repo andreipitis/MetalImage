50fc8dd7c774bcfaa25340c7c0c6a776cccf8460
Andrei-Sergiu Pițiș <andrei.pitis@lateral-inc.com>
2018-01-28 20:17:50 +0200

    Initial Commit

---
+++ MetalImageTests/MetalImageTests.swift
@@ -0,0 +1,36 @@
+//
+//  MetalImageTests.swift
+//  MetalImageTests
+//
+//  Created by Andrei-Sergiu Pițiș on 28/01/2018.
+//  Copyright © 2018 Andrei-Sergiu Pițiș. All rights reserved.
+//
+
+import XCTest
+@testable import MetalImage
+
+class MetalImageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
